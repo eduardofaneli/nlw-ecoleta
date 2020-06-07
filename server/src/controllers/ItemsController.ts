43fdd9f90771ca958281ee4c22dd31cab6955090
eduardofaneli <eduardofaneli2@gmail.com>
2020-06-07 13:57:18 -0300

    app

---
+++ server/src/controllers/ItemsController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express';
+import knex from '../database/connection';
+
+class ItemsController {
+  async index(request: Request, response: Response) {
+    const items = await knex('items').select('*');
+
+    const serializedItems = items.map(items => {
+      return {
+        id: items.id,
+        title: items.title,
+        image_url: `http://192.168.1.101:3333/uploads/${items.image}`
+      }
+    })
+
+    return response.json(serializedItems);
+  }
+}
+
+export default ItemsController;
